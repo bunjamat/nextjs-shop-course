c195642bfa48b0800cfee0edc7ae9dc2fa67ec0c
bunjamat <bunjamat@gmail.com>
2023-06-10 16:05:21 +0700

    init

---
+++ app/api/product/new/route.js
@@ -0,0 +1,32 @@
+import prisma from "@/lib/prisma";
+
+export const POST = async (request) => {
+
+  const { name, price,stock,image,active } = await request.json();
+
+  try {
+    const newData = {
+        name : name,
+        price : price,
+        stock : stock,
+        image : image,
+        active : active
+    }
+    
+    const newProduct = await prisma.product.create({
+        data : newData
+    });
+
+    return new Response(JSON.stringify(newProduct), { status: 201 });
+
+  } catch (error) {
+
+    console.log("🚀 ~ file: route.js:44 ~ POST ~ error:", error);
+
+
+    return new Response(
+      JSON.stringify({ error: "ไม่สามารถสร้าง สินค้าได้" }),
+      { status: 500 }
+    );
+  }
+};

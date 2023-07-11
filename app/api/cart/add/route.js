db8f9d606ece9c9b0b10505c68169f6cacbb6084
thongchai bunjamat <bunjamat@gmail.com>
2023-07-11 17:41:33 +0700

    Merge branch 'main'

--- app/api/cart/add/route.js
+++ app/api/cart/add/route.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 import prisma from "@/lib/prisma";
 
 export const POST = async (request) => {
   const { user_id, product_id, product_name, product_price, qty } =
     await request.json();
 
-=======
-//เชื่อม database
-import prisma from "@/lib/prisma";
-
-//สร้าง method รับ POST
-export const POST = async (request) => {
-  //request มาแยกเป็น object
-  //รับเป็น request ที่ส่งมา
-  const { user_id, product_id, product_name, product_price, qty } =
-    await request.json();
->>>>>>> 9e2e3ef7a5dc7d622028e36e8abd0c9d0f762bc0
   try {
     const newData = {
       user_id: parseInt(user_id),
@@ -25,33 +13,6 @@ export const POST = async (request) => {
       qty: qty,
     };
 
-<<<<<<< HEAD
-    // ค้นหาว่ามีแล้วหรือยัง
-    const existingProduct = await prisma.cart.findFirst({
-      where: { user_id: parseInt(user_id), product_id: product_id },
-    });
-    // console.log("🚀 ~ file: route.js:20 ~ POST ~ existingProduct:", existingProduct)
-
-    if (!existingProduct) {
-      const newCart = await prisma.cart.create({
-        data: newData,
-      });
-      return new Response(JSON.stringify(newCart), { status: 201 });
-    }
-
-    // Update the prompt with new data
-    existingProduct.qty = existingProduct.qty += qty;
-    // console.log("🚀 ~ file: route.js:30 ~ POST ~ existingProduct:", existingProduct)
-
-    const updatedCart = await prisma.cart.update({
-      where: { id: parseInt(existingProduct.id) },
-      data: existingProduct,
-    });
-    return new Response(JSON.stringify(updatedCart), { status: 200 });
-  } catch (error) {
-    console.log("🚀 ~ file: route.js:44 ~ POST ~ error:", error);
-
-=======
     //ค้นหาว่ามีในตระกร้ารึยัง
     const existingProduct = await prisma.cart.findFirst({
       where: { user_id: parseInt(user_id), product_id: product_id },
@@ -77,7 +38,6 @@ export const POST = async (request) => {
     
   } catch (error) {
     console.log("🚀 ~ file: route.js:39 ~ POST ~ error:", error);
->>>>>>> 9e2e3ef7a5dc7d622028e36e8abd0c9d0f762bc0
     return new Response(
       JSON.stringify({ error: "ไม่สามารถเพิ่มเข้าตระกร้าได้" }),
       {
--- app/api/category/route.js
+++ app/api/category/route.js
@@ -2,23 +2,11 @@ import prisma from "@/lib/prisma";
 
 export const GET = async (request) => {
   try {
-<<<<<<< HEAD
-    const category = await prisma.category.findMany(/* {
-      include: {
-        product: true,
-      },
-    } */);
-    return new Response(JSON.stringify(category), { status: 201 });
-  } catch (error) {
-    console.log("🚀 ~ file: route.js:44 ~ POST ~ error:", error);
-
-=======
     const allCategory = await prisma.category.findMany();
 
     return new Response(JSON.stringify(allCategory), { status: 201 });
   } catch (error) {
     console.log("🚀 ~ file: route.js:44 ~ POST ~ error:", error);
->>>>>>> 9e2e3ef7a5dc7d622028e36e8abd0c9d0f762bc0
     return new Response(JSON.stringify({ error: "ไม่สามารถ ดึงข้อมูลได้" }), {
       status: 500,
     });
